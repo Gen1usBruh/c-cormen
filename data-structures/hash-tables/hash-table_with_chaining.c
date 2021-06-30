ffa6a5855eb4f872ada9ad0065e8e5c463f811d5
Gen1usBruh <75973636+Gen1usBruh@users.noreply.github.com>
2021-06-30 09:57:50 +0500

    Update hash-table_with_chaining.c

--- data-structures/hash-tables/hash-table_with_chaining.c
+++ data-structures/hash-tables/hash-table_with_chaining.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Simple implementation of hash table with chaining using doubly linked list and array of constant size
+// No operation (function) provided supports error-checking
+
 #define MAXSIZE 23
 
 typedef struct Node{
@@ -14,31 +17,42 @@ node* table[MAXSIZE] = {NULL};
 unsigned hash(char* key);
 void insertNodeAtEnd(node **head, int value);
 void insert(char* key, int value);
+node* search(unsigned key);
+void delete(char* key);
 void display(void);
 
 
 int main(int argc, char* argv[]){
     
-    insert("amir", 12);
-    insert("mapelle", 13);
-    insert("azim", 17);
-    insert("dfgdfg", 19);
-    insert("usyuvfs", 15);
-    insert("amidfg", 12);
-    insert("mapeldsfle", 13);
-    insert("azidsfm", 17);
-    insert("dfgddfgfg", 19);
-    insert("usyuvdfvfs", 15);
-    insert("amsdfir", 12);
-    insert("mapsredfelle", 13);
-    insert("azimfdg", 17);
-    insert("dfewtgdfg", 19);
+    insert("amir", 1);
+    insert("mapelle", 2);
+    insert("azim", 3);
+    insert("dfgdfg", 4);
+    insert("usyuvfs", 5);
+    insert("amidfg", 6);
+    insert("mapeldsfle", 7);
+    insert("azidsfm", 8);
+    insert("dfgddfgfg", 9);
+    insert("usyuvdfvfs", 10);
+    insert("amsdfir", 11);
+    insert("mapsredfelle", 12);
+    insert("azimfdg", 13);
+    insert("dfewtgdfg", 14);
     insert("usyxcvbuvfs", 15);
-    insert("amirfdg", 12);
-    insert("mapserelle", 13);
-    insert("azidgm", 17);
+    insert("amirfdg", 16);
+    insert("mapserelle", 17);
+    insert("azidgm", 18);
     insert("dfgdhdfsdfg", 19);
-    insert("usywwruvfs", 15);
+    insert("usywwruvfs", 20);
+
+    display();
+    
+    printf("\n\n");
+
+    delete("azim");
+    delete("dfgddfgfg");
+
+    printf("\n\n");
 
     display();
 
@@ -73,7 +87,7 @@ void insertNodeAtEnd(node **head, int value){
 void insert(char* key, int value){
     printf("User key: %s --> ", key);
     unsigned hashedKey = hash(key);
-    printf("It's hash: %d\n", key);
+    printf("It's hash: %d\n", hashedKey);
     if(table[hashedKey] == NULL){
         node* slotHead = NULL;
         insertNodeAtEnd(&slotHead, value);
@@ -82,6 +96,46 @@ void insert(char* key, int value){
     else{
         insertNodeAtEnd(&table[hashedKey], value);
     }
+}
+
+void delete(char* key){
+    unsigned hashedKey = hash(key);
+    node* temp = table[hashedKey];
+    if(temp->next == NULL){
+        table[hashedKey] = NULL;
+        free(temp);
+    }
+    else{
+        int num;
+        printf("Enter the value of the key: ");
+        scanf("%d", &num);
+        temp = table[hashedKey];
+        for(; temp != NULL; temp = temp->next){
+            if(temp->data == num){
+                break;
+            }
+        }
+        if(temp == NULL){
+            printf("No such value");
+        }
+        else if(temp == table[hashedKey]){
+            temp->next->prev = NULL;
+            table[hashedKey] = temp->next;
+            temp->next = NULL;
+        }
+        else if(temp->next == NULL){
+            temp->prev->next = NULL;
+            temp->prev = NULL;
+        }
+        else{
+            temp->prev->next = temp->next;
+            temp->next->prev = temp->prev;
+            temp->next = temp->prev = NULL;
+        }
+        free(temp);
+    }
+
+    printf("\n\nDeletion completed!\n\n");
 }
 
 void display(void){
