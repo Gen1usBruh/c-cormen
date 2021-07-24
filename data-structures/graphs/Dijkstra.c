0493441441038df8e3cc22086cba314d009e8071
Gen1usBruh <75973636+Gen1usBruh@users.noreply.github.com>
2021-07-24 15:16:21 +0500

    Create Dijkstra.c

---
+++ data-structures/graphs/Dijkstra.c
@@ -0,0 +1,218 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <inttypes.h>
+
+
+
+#define CAPACITY 10
+
+typedef struct Node {
+    int data;
+    long long upperBound;
+    int weight;
+    struct Node* parent;
+    struct Node* next;
+    struct Node* prev;
+} node;
+
+typedef struct Graph {
+    node** adjl;   // adjacent list
+} graph;
+
+graph* newGraph() {
+    graph* initGraph = malloc(sizeof(graph));
+    initGraph->adjl = malloc(sizeof(node*) * CAPACITY);
+    for (int i = 0; i < CAPACITY; i++) {
+        initGraph->adjl[i] = NULL;
+    }
+
+    return initGraph;
+}
+
+void insertNodeAtEnd(node** head, int value, int weight_) {
+    node* new_node = malloc(sizeof(node));
+    node* last = *head;
+    new_node->data = value;
+    new_node->weight = weight_;
+    new_node->next = NULL;
+    if (*head == NULL) {
+        new_node->prev = NULL;
+        *head = new_node;
+        return;
+    }
+    while (last->next != NULL) {
+        last = last->next;
+    }
+    new_node->prev = last;
+    last->next = new_node;
+}
+
+void insert(graph* graph, int vertexIndex, int vertexAdj, int weight_) {
+    if (graph->adjl[vertexIndex] == NULL) {
+        insertNodeAtEnd(&graph->adjl[vertexIndex], vertexIndex, 0);
+    }
+    if (graph->adjl[vertexAdj] == NULL) {
+        insertNodeAtEnd(&graph->adjl[vertexAdj], vertexAdj, 0);
+    }
+    insertNodeAtEnd(&graph->adjl[vertexIndex], vertexAdj, weight_);
+}
+
+void initSingleSource(graph* graph, int source) {
+    for (int i = 0; i < CAPACITY; i++) {
+        if (graph->adjl[i] != NULL) {
+            graph->adjl[i]->upperBound = INT_MAX;
+            graph->adjl[i]->parent = NULL;
+        }
+    }
+    graph->adjl[source]->upperBound = 0;
+}
+
+void relax(graph* graph, node* u, node* v) {
+    if (graph->adjl[v->data]->upperBound > u->upperBound + v->weight) {
+        graph->adjl[v->data]->upperBound = u->upperBound + v->weight;
+        graph->adjl[v->data]->parent = u;
+    }
+}
+
+void dumpGraph(graph* graph) {
+    for (int i = 1; i < CAPACITY; ++i) {
+        node* entry = graph->adjl[i];
+        if (entry == NULL) {
+            continue;
+        }
+        printf("V(%d): ", i);
+        for (;;) {
+            entry = entry->next;
+            if (entry == NULL) {
+                break;
+            }
+            printf("%d ", entry->data);
+        }
+        printf("\n  |___");
+        printf("UpperBound : %"PRId64"", graph->adjl[i]->upperBound);
+        if (graph->adjl[i]->parent != NULL) {
+            printf(" | Parent : %d", graph->adjl[i]->parent->data);
+        }
+        printf("\n\n");
+    }
+}
+
+
+void minHeapify(node** a, int i, int length) {
+    int l = (i << 1) + 1;
+    int r = (i << 1) + 2;
+    int smallest = INT_MIN;
+    if (l < length && a[l]->upperBound < a[i]->upperBound) {
+        smallest = l;
+    }
+    else {
+        smallest = i;
+    }
+    if (r < length && a[r]->upperBound < a[smallest]->upperBound) {
+        smallest = r;
+    }
+    if (smallest != i) {
+        node* tmp = a[i];
+        a[i] = a[smallest];
+        a[smallest] = tmp;
+        minHeapify(a, smallest, length);
+    }
+}
+
+void buildMinHeap(node** a, int length) {
+    for (int i = length / 2; i >= 0; i--) {
+        minHeapify(a, i, length);
+    }
+}
+
+node* heapExtractMin(node** a, int length) {
+    if (length < 1) {
+        fprintf(stderr, "Heap underflow");
+        exit(EXIT_FAILURE);
+    }
+    node* min = a[0];
+    a[0] = a[length - 1];
+    --length;
+    minHeapify(a, 0, length);
+
+    return min;
+}
+
+int isEmptyHeap(node** heap, int n) {
+    for (int i = 0; i < n; i++) {
+        if (heap[i] != NULL) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int numOfV(graph* graph) {
+    int j = 0;
+    for (int i = 0; i < CAPACITY; i++) {
+        if (graph->adjl[i] != NULL) {
+            ++j;
+        }
+    }
+    return j;
+}
+
+void dijkstra(graph* graph, int source) {
+    initSingleSource(graph, source);
+    int numVer = numOfV(graph);
+    node* S[numVer];  
+    node* heap[numVer];   
+    for (int i = 0, j = 0; i < CAPACITY; i++) {
+        if (graph->adjl[i] != NULL) {
+            heap[j] = graph->adjl[i];
+            ++j;
+        }
+    }
+    buildMinHeap(heap, numVer);
+    int k = 0;
+    while (!isEmptyHeap(heap, numVer)) {
+        node* u = heapExtractMin(heap, numVer);
+        --numVer;
+        S[k] = u;
+        ++k;
+        for (node* curr = u->next; curr != NULL; curr = curr->next) {
+            relax(graph, u, curr);
+        }
+    }
+}
+
+
+int main(int argc, char* argv[]) {
+    graph* myGraph = newGraph();
+
+    // insert(myGraph, 1, 3, 8);   // Edge from 1 to 3 only
+    // insert(myGraph, 1, 5, 5);
+    // insert(myGraph, 1, 9, 4);
+    // insert(myGraph, 3, 5, 3);
+    // insert(myGraph, 3, 9, 9);   // --> Directed graph test
+    // insert(myGraph, 5, 1, 2);
+    // insert(myGraph, 7, 1, 6);
+    // insert(myGraph, 7, 3, 7);
+    // insert(myGraph, 9, 5, 7);
+    // insert(myGraph, 9, 7, 2);
+
+    insert(myGraph, 1, 3, 2);   // Edge from 1 to 3 only
+    insert(myGraph, 1, 5, 1);
+    insert(myGraph, 3, 1, 3);
+    insert(myGraph, 3, 5, 9);
+    insert(myGraph, 3, 9, 2);   // --> Directed graph test
+    insert(myGraph, 5, 9, 4);
+    insert(myGraph, 7, 1, 10);
+    insert(myGraph, 7, 3, 5);
+    insert(myGraph, 9, 5, 6);
+    insert(myGraph, 9, 7, 7);
+
+    // if you need a vertex in directed graph that points to nowhere, or in undirected graph a totally separate vertex, then:
+    // insertNodeAtEnd(&myGraph->adjl[i], i), where i should be the of the same value 
+
+    dijkstra(myGraph, 7);
+
+    dumpGraph(myGraph);
+
+}
